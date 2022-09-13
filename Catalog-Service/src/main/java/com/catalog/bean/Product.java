cadb7755e333e840ef73b828e9b14d00656edbed
suyesh.kumar123@gmail.com <suyesh.kumar123@gmail.com>
2022-09-13 16:19:08 +0530

    Skeleton completed

---
+++ Catalog-Service/src/main/java/com/catalog/bean/Product.java
@@ -0,0 +1,19 @@
+package com.catalog.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+    private double price;
+
+
+}
