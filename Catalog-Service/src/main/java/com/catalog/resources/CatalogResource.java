6c8b4b42d9ff72b899615e24967fb95a8f1e3958
suyesh.kumar123@gmail.com <suyesh.kumar123@gmail.com>
2022-09-13 19:51:37 +0530

    Service and resources completed

--- Catalog-Service/src/main/java/com/catalog/resources/CatalogResource.java
+++ Catalog-Service/src/main/java/com/catalog/resources/CatalogResource.java
@@ -1,8 +1,30 @@
 package com.catalog.resources;
 
+import org.springframework.http.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.catalog.bean.Product;
+import com.catalog.bean.ProductList;
+import com.catalog.service.CatalogService;
 
 @RestController
 public class CatalogResource {
+	
+	@Autowired
+	private CatalogService catalogService;
+	
+	
+	@GetMapping(path = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProductList listAllProducts() {
+		return catalogService.listAllProducts();
+	}
+	@GetMapping(path = "/products/code/{productCode}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Product getProductByCode(@PathVariable("productCode") String productCode) {
+		return catalogService.getProductByCode(productCode);
+	}
 
 }
--- Catalog-Service/src/main/java/com/catalog/service/CatalogServiceImpl.java
+++ Catalog-Service/src/main/java/com/catalog/service/CatalogServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.catalog.bean.Product;
 import com.catalog.bean.ProductList;
 import com.catalog.repository.CatalogDao;
 
+@Service
 public class CatalogServiceImpl implements CatalogService{
 
 	@Autowired
