6cc4c4cf5dcd520285645fd021ea68925304b4cf
suyesh.kumar123@gmail.com <suyesh.kumar123@gmail.com>
2022-09-13 16:44:45 +0530

    TCER created

--- Catalog-Service/src/main/java/com/catalog/main/CatalogServiceApplication.java
+++ Catalog-Service/src/main/java/com/catalog/main/CatalogServiceApplication.java
@@ -2,8 +2,14 @@ package com.catalog.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.catalog")
+@EntityScan(basePackages = "com.catalog.bean")
+@EnableJpaRepositories(basePackages = "com.catalog.repository")
+@EnableEurekaClient
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
